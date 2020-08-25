185fe93f6e7bade2007895c9012b142066406966
Przemysław Górecki <przemyslaw@toptal.com>
2020-08-25 16:12:50 +0200

    cleanup code

--- src/pages/eligibilityQueue/EligibilityQueue.js
+++ src/pages/eligibilityQueue/EligibilityQueue.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
-import { Grid, Tab, Button, Segment } from 'semantic-ui-react';
-import { EligibilityLabel, ErrorMessage } from 'components/common';
+import { Tab, Button, Segment } from 'semantic-ui-react';
+import { EligibilityLabel } from 'components/common';
 import toaster from 'components/toaster';
-import {
-  EditActionLink,
-  DeleteActionButton,
-  PrimaryActionLink,
-} from 'components/tableComponents';
-import useApiClient from 'hooks/useApiClient';
-import { formatDateTime } from 'utils/typeUtils';
-import { formatUser } from 'utils/modelUtils';
-import { formatApiError } from 'utils/apiUtils';
-import ListPage from '../ListPage';
 import PaginatedDataTable from 'components/PaginatedDataTable';
-import usePaginatedDataTable from 'hooks/usePaginatedDataTable';
+import useApiClient from 'hooks/useApiClient';
 import useUrlParams from 'hooks/useUrlParams';
-import { clientFullName } from 'utils/modelUtils';
+import usePaginatedDataTable from 'hooks/usePaginatedDataTable';
+import { formatDateTime } from 'utils/typeUtils';
+import { formatApiError } from 'utils/apiUtils';
+import { clientFullName, formatUser } from 'utils/modelUtils';
+import ListPage from '../ListPage';
 
 function NewClientsTab() {
   const apiClient = useApiClient();
@@ -245,99 +239,4 @@ export default function EligilibityQueueList() {
       />
     </ListPage>
   );
-
-  /*
-  const table = usePaginatedDataTable({ url: '/eligibility/queue/' });
-  const apiClient = useApiClient();
-  const [modalData, setModaData] = useState({});
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        accessor: 'name',
-        Cell: ({ value, row }) => (
-          <NavLink to={`/surveys/${row.original.id}`}>{value}</NavLink>
-        ),
-      },
-      {
-        Header: 'Access',
-        accessor: 'is_public',
-        Cell: ({ value }) => <Label>{value ? 'Public' : 'Private'}</Label>,
-      },
-      {
-        Header: 'Date Created',
-        accessor: 'created_at',
-        Cell: ({ value }) => formatDateTime(value, true),
-      },
-      {
-        Header: 'Date Modified',
-        accessor: 'modified_at',
-        Cell: ({ value }) => formatDateTime(value, true),
-      },
-      {
-        Header: 'Created By',
-        accessor: 'created_by',
-        Cell: ({ value }) => formatOwner(value),
-      },
-      {
-        Header: 'Actions',
-        accessor: 'actions',
-        Cell: ({ row, actions }) => (
-          <>
-            <EditActionLink to={`/surveys/${row.original.id}/edit`} />
-            <PrimaryActionLink
-              icon="table"
-              label="Builder"
-              to={`/surveys/${row.original.id}/builder`}
-              disabled
-            />
-            <DeleteActionButton
-              onClick={() => setModaData({ ...row.original })}
-            />
-          </>
-        ),
-      },
-    ],
-    []
-  );
-
-  return (
-    <ListPage title="Surveys">
-      <Button primary as={NavLink} exact to="/surveys/new">
-        New Survey
-      </Button>
-      <PaginatedDataTable columns={columns} table={table} />
-      <Modal closeIcon open={!!modalData.id} onClose={() => setModaData({})}>
-        <Modal.Header>Are you sure?</Modal.Header>
-        <Modal.Content>
-          <Modal.Description>
-            <p>
-              Are you sure you want to delete survey{' '}
-              <strong>{modalData.name}</strong>?
-            </p>
-          </Modal.Description>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button
-            negative
-            onClick={async () => {
-              try {
-                await apiClient.delete(`/surveys/${modalData.id}/`);
-                table.reload();
-              } catch (err) {
-                const apiError = formatApiError(err.response);
-                toaster.error(apiError);
-              } finally {
-                setModaData({});
-              }
-            }}
-          >
-            Delete Survey
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    </ListPage>
-  );
-  */
 }
