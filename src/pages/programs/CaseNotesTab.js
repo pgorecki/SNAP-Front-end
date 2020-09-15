cf32c92b47f57bb032d276fb333c4bedab39b530
ashar-ilyas <ilyas.ashar@gmail.com>
2020-09-16 00:28:25 +0530

    Fixed update case note issue
    Fixed update case note issue

--- src/pages/programs/CaseNotesTab.js
+++ src/pages/programs/CaseNotesTab.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Header, Form, Grid, Modal, Tab, Loader, Message } from 'semantic-ui-react';
+import {
+  Button,
+  Header,
+  Form,
+  Grid,
+  Modal,
+  Tab,
+  Loader,
+  Message,
+} from 'semantic-ui-react';
 import { Formik } from 'formik';
 import useApiClient from 'hooks/useApiClient';
 import useResourceIndex from 'hooks/useResourceIndex';
@@ -14,11 +23,20 @@ import { formatDateTime, FieldError, formatDate } from 'utils/typeUtils';
 import toaster from 'components/toaster';
 import EnrollmentSurveyModal from 'modals/EnrollmentSurveyModal';
 import useFetchData from 'hooks/useFetchData';
-import { FormSelect, FormDatePicker, FormErrors, FormInput, FormTextArea } from 'components/FormFields';
-import { EditActionLink, DeleteActionButton, EditActionButton } from '../../components/tableComponents';
+import {
+  FormSelect,
+  FormDatePicker,
+  FormErrors,
+  FormInput,
+  FormTextArea,
+} from 'components/FormFields';
+import {
+  EditActionLink,
+  DeleteActionButton,
+  EditActionButton,
+} from '../../components/tableComponents';
 import ListPage from '../ListPage';
 import { formatOwner } from '../../utils/modelUtils';
-
 
 export default function CaseNotesTab({ enrollData }) {
   //console.log(enrollData);
@@ -28,7 +46,7 @@ export default function CaseNotesTab({ enrollData }) {
   const table = usePaginatedDataTable({
     url: `/notes/?source_id=${enrollData.id}`,
   });
-  //console.log(table);
+  console.log(table);
   const { save } = useNewResource('/notes/', {});
   const { data, ready, error } = programsIndex;
   //Modal related
@@ -40,27 +58,23 @@ export default function CaseNotesTab({ enrollData }) {
   //console.log(modalDataEd);
   const [initialValues, setInitialValues] = useState({
     source: {
-      id: enrollData.id
-      , type: 'Enrollment'
-    }
+      id: enrollData.id,
+      type: 'Enrollment',
+    },
     //,text:'a message'
-  }
-  );
+  });
   const options = data
     ? data.map(({ id, name }) => ({
-      value: id,
-      text: name,
-    }))
+        value: id,
+        text: name,
+      }))
     : [];
   //console.log(options);
   const cncolumns = React.useMemo(
     () => [
       {
         Header: 'Case Note Type',
-        accessor: 'source.type',
-        Cell: ({ value, row }) => (
-          <NavLink to={`/notes/${row.original.id}`}>{value}</NavLink>
-        ),
+        accessor: 'text',
       },
       {
         Header: 'Date',
@@ -77,9 +91,12 @@ export default function CaseNotesTab({ enrollData }) {
         accessor: 'actions',
         Cell: ({ row }) => (
           <>
-
-            <EditActionButton onClick={() => setModaDataEd({ ...row.original })} />
-            <DeleteActionButton onClick={() => setModaData({ ...row.original })} />
+            <EditActionButton
+              onClick={() => setModaDataEd({ ...row.original })}
+            />
+            <DeleteActionButton
+              onClick={() => setModaData({ ...row.original })}
+            />
           </>
         ),
       },
@@ -91,17 +108,18 @@ export default function CaseNotesTab({ enrollData }) {
     <>
       <Button primary onClick={handleShow}>
         New Case Note
-          </Button>
+      </Button>
       <PaginatedDataTable columns={cncolumns} table={table} />
       <Formik
         enableReinitialize
         initialValues={initialValues}
-        onSubmit={async (values, actions) => {debugger;
+        onSubmit={async (values, actions) => {
+          debugger;
           try {
             const result = await save({
               ...values,
               title: values.subject,
-              text: values.noteDesc
+              text: values.noteDesc,
             });
             //history.push(`/notes/${result.id}`);
             toaster.success('Notes created');
@@ -118,8 +136,7 @@ export default function CaseNotesTab({ enrollData }) {
             return null;
           }
           const selectedProgram =
-            data &&
-            data.find((program) => program.id === form.values.program);
+            data && data.find((program) => program.id === form.values.program);
 
           let intakeSurvey = null;
           if (selectedProgram) {
@@ -129,19 +146,28 @@ export default function CaseNotesTab({ enrollData }) {
           return (
             <>
               <Modal open={show} onHide={handleClose}>
-                <Modal.Header>
-                  New Case Note
-                        </Modal.Header>
+                <Modal.Header>New Case Note</Modal.Header>
                 <Modal.Content>
                   <Form error onSubmit={form.handleSubmit}>
                     <FormInput label="Subject:" name="subject" form={form} />
-                    <FormSelect label="Select Template" name="template" form={form} options={options} placeholder="Select Template" disabled="true" />
+                    <FormSelect
+                      label="Select Template"
+                      name="template"
+                      form={form}
+                      options={options}
+                      placeholder="Select Template"
+                      disabled="true"
+                    />
                     <FormDatePicker label="Date" name="date" form={form} />
-                    <FormTextArea name="noteDesc" placeholder="Enter note here" form={form} />
+                    <FormTextArea
+                      name="noteDesc"
+                      placeholder="Enter note here"
+                      form={form}
+                    />
                     <FormErrors form={form} />
                     <Button primary type="submit" disabled={form.isSubmitting}>
                       Submit
-                                </Button>
+                    </Button>
                     <Button onClick={handleClose}>Cancel</Button>
                   </Form>
                 </Modal.Content>
@@ -157,7 +183,7 @@ export default function CaseNotesTab({ enrollData }) {
             <p>
               Are you sure you want to delete note{' '}
               <strong>{modalData.text}</strong>?
-                </p>
+            </p>
           </Modal.Description>
         </Modal.Content>
         <Modal.Actions>
@@ -176,11 +202,15 @@ export default function CaseNotesTab({ enrollData }) {
             }}
           >
             Delete Note
-              </Button>
+          </Button>
         </Modal.Actions>
       </Modal>
 
-      <Modal closeIcon open={!!modalDataEd.id} onClose={() => setModaDataEd({})}>
+      <Modal
+        closeIcon
+        open={!!modalDataEd.id}
+        onClose={() => setModaDataEd({})}
+      >
         <Modal.Header>Edit Case Note</Modal.Header>
         <Modal.Content>
           <Formik
@@ -188,11 +218,16 @@ export default function CaseNotesTab({ enrollData }) {
             initialValues={modalDataEd}
             onSubmit={async (values, actions) => {
               try {
-                const result = await apiClient.patch({
-                  ...values,
-                  title: values.subject,
-                  text: values.note
+                debugger;
+                await apiClient.patch(`/notes/${values.id}/`, {
+                  source: {
+                    id: values.source.id,
+                    type: 'Enrollment',
+                  },
+                  title: values.title,
+                  text: values.text,
                 });
+                setModaDataEd({});
                 toaster.success('Notes updated');
               } catch (err) {
                 actions.setErrors(apiErrorToFormError(err));
@@ -205,10 +240,44 @@ export default function CaseNotesTab({ enrollData }) {
             {(formEdit) => {
               return (
                 <>
-                  <Form error initialValues={modalDataEd} onSubmit={formEdit.handleSubmit}>
-                    <FormInput label="Subject:" name="subject" form={formEdit} value={modalDataEd.title} />
+                  <Form
+                    error
+                    initialValues={modalDataEd}
+                    onSubmit={formEdit.handleSubmit}
+                  >
+                    <FormInput
+                      label="Subject:"
+                      name="subject"
+                      form={formEdit}
+                      value={modalDataEd.title}
+                      onChange={(e) =>
+                        setModaDataEd({ ...modalDataEd, title: e.target.value })
+                      }
+                    />
                     <FormDatePicker label="Date" name="date" form={formEdit} />
-                    <FormTextArea name="note" placeholder="Enter note here" form={formEdit} value={modalDataEd.text}  />
+                    <FormTextArea
+                      name="note"
+                      placeholder="Enter note here"
+                      form={formEdit}
+                      value={modalDataEd.text}
+                      onChange={(e) =>
+                        setModaDataEd({ ...modalDataEd, text: e.target.value })
+                      }
+                    />
+                    <Button
+                      primary
+                      type="submits"
+                      disabled={formEdit.isSubmitting}
+                    >
+                      Submit
+                    </Button>
+                    <Button
+                      onClick={async () => {
+                        setModaDataEd({});
+                      }}
+                    >
+                      Cancel
+                    </Button>
                     <FormErrors form={formEdit} />
                   </Form>
                 </>
@@ -216,27 +285,6 @@ export default function CaseNotesTab({ enrollData }) {
             }}
           </Formik>
         </Modal.Content>
-        <Modal.Actions>
-          <Button
-            primary
-            onClick={async () => {
-              try {
-                await apiClient.edit(`/notes/${modalDataEd.id}/`);
-                table.reload();
-              } catch (err) {
-                const apiError = formatApiError(err.response);
-                toaster.error(apiError);
-              } finally {
-                setModaDataEd({});
-              }
-            }}
-          >
-            Save
-              </Button>
-          <Button onClick={async () => { setModaDataEd({}); }}>
-            Cancel
-              </Button>
-        </Modal.Actions>
       </Modal>
     </>
   );
